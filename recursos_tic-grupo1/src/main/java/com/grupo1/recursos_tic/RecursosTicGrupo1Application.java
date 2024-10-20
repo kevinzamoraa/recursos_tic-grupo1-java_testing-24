5c40410c68280ce183ef85c760f5138329220850
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-10-20 20:21:59 +0200

    init repo / adding project base structure

---
+++ recursos_tic-grupo1/src/main/java/com/grupo1/recursos_tic/RecursosTicGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.grupo1.recursos_tic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosTicGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosTicGrupo1Application.class, args);
+	}
+
+}
---
+++ recursos_tic-grupo1/src/test/java/com/grupo1/recursos_tic/RecursosTicGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.recursos_tic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosTicGrupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
