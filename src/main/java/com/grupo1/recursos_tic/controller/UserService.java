6441dbdbb810c01b533103b4f519a9a00cf82f30
KevinZamora <kevin.zamora.amela@gmail.com>
2024-11-09 17:12:35 +0100

    feature / user's delete method starting with linked objects deletion

--- src/main/java/com/grupo1/recursos_tic/controller/UserService.java
+++ src/main/java/com/grupo1/recursos_tic/controller/UserService.java
@@ -2,6 +2,7 @@ package com.grupo1.recursos_tic.controller;
 
 import com.grupo1.recursos_tic.model.Rating;
 import com.grupo1.recursos_tic.model.User;
+import com.grupo1.recursos_tic.repository.ResourceListsRepo;
 import com.grupo1.recursos_tic.repository.UserRepo;
 import com.grupo1.recursos_tic.repository.RatingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class UserService {
     private UserRepo userRepository;
 
     @Autowired
+    private ResourceListsRepo resourceListRepository;
+
+    @Autowired
     private RatingRepo ratingRepository;
 
     public void deleteUserWithRatings(Long userId) {
@@ -24,11 +28,18 @@ public class UserService {
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
             try {
-                // Eliminar ratings asociados al usuario
+                // Eliminar 'ratings' asociados al usuario
                 int deletedCount = ratingRepository.deleteRatingByUserId(user.getId());
                     System.out.println("Se han eliminado " + deletedCount + " ratings.");
             } catch (Exception e) {
                 throw new RuntimeException("Error al eliminar ratings", e);
+            }
+            try {
+                // Eliminar 'resourceList' asociadas al usuario
+                resourceListRepository.deleteResourceListByUserId(user.getId());
+                System.out.println("Se han eliminado los recursos creados por el usuario a borrar.");
+            } catch (Exception e) {
+                throw new RuntimeException("Error al eliminar el usuario", e);
             }
             try {
                 // Eliminar el usuario
--- src/main/java/com/grupo1/recursos_tic/repository/ResourceListsRepo.java
+++ src/main/java/com/grupo1/recursos_tic/repository/ResourceListsRepo.java
@@ -3,5 +3,16 @@ package com.grupo1.recursos_tic.repository;
 import com.grupo1.recursos_tic.model.ResourceList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ResourceListsRepo extends JpaRepository<ResourceList, Long> {}
+public interface ResourceListsRepo extends JpaRepository<ResourceList, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ResourceList rl WHERE rl.owner.id = :id")
+    void deleteResourceListByUserId(@Param("id") Long id);
+
+}
