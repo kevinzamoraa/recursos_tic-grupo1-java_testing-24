02f7e634453f88b1b8dc75cbda66e19991541541
JavGuerra <javguerra@badared.com>
2024-11-12 11:54:03 +0100

    Listar solamente los resourceLists del usuario autenticado.

--- src/main/java/com/grupo1/recursos_tic/Main.java
+++ src/main/java/com/grupo1/recursos_tic/Main.java
@@ -19,37 +19,52 @@ public class Main {
 
 		var context = SpringApplication.run(Main.class, args);
 
+
+		// Users
 		UserRepo userRepository = context.getBean(UserRepo.class);
 		var passwordEncoder = context.getBean(PasswordEncoder.class);
 
-		var user1 = User.builder().name("Javi").email("a@a.es").username("javi")
-				.password(passwordEncoder.encode("AbCd4321")).build();
-		var user2 = User.builder().name("Marina").email("b@b.es").username("marina")
-				.password(passwordEncoder.encode("DcBa1234")).build();
-		var user3 = User.builder().name("Kevin").email("c@c.es").username("kevin")
-				.password(passwordEncoder.encode("DcBa1234")).build();
-		var admin = User.builder().name("Admin").email("admin@admin.es").username("admin")
+		var admin = User.builder().name("Administrador").email("admin@admin.es").username("admin")
 				.password(passwordEncoder.encode("admin1234")).build();
-		userRepository.saveAll(List.of(user1, user2, user3, admin));
+		var user1 = User.builder().name("Javier").email("a@a.es").username("javier")
+				.password(passwordEncoder.encode("AbCd4321")).build();
+		var user2 = User.builder().name("Kevin").email("b@b.es").username("kevin")
+				.password(passwordEncoder.encode("DcBa1234")).build();
+		var user3 = User.builder().name("Marina").email("c@c.es").username("marina")
+				.password(passwordEncoder.encode("DcBa1234")).build();
 
+		userRepository.saveAll(List.of(admin, user1, user2, user3));
+
+
+		// Resources
 		ResourceRepo resourceRepository = context.getBean(ResourceRepo.class);
+
 		var resource1 = Resource.builder().title("Libro 1").type(ResourceType.BOOK)
 				.author("Autor1").description("Descripción del libro 1").url("#").build();
 		var resource2 = Resource.builder().title("Libro 2").type(ResourceType.VIDEO)
 				.author("Autor2").description("Descripción del libro 2").url("#").build();
+
 		resourceRepository.saveAll(List.of(resource1, resource2));
 
+
+		// Resource lists
 		ResourceListsRepo resourceListsRepository = context.getBean(ResourceListsRepo.class);
+
 		var resourceList1 = ResourceList.builder().owner(user1).name("Lista 1")
 				.description("Descripción de la lista 1")
 				.resources(Set.of(resource1, resource2)).build();
+
 		resourceListsRepository.save(resourceList1);
 
+
+		// Ratings
 		var ratingRepository = context.getBean(RatingRepo.class);
+
 		var rating1 = Rating.builder().user(user1).resource(resource2)
 				.comment("Comentario de prueba 1").score(3).build();
 		var rating2 = Rating.builder().user(user2).resource(resource1)
 				.comment("Comentario de prueba 2").score(3).build();
+
 		ratingRepository.saveAll(List.of(rating1, rating2));
 	}
 
