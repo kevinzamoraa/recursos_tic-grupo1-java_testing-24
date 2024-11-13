8712a278b7b61f0b47145c7230cf59cc0cc3f3c9
JavGuerra <javguerra@badared.com>
2024-11-14 00:22:10 +0100

    Tipos de recursos y etiquetas.

--- src/main/java/com/grupo1/recursos_tic/model/ResourceType.java
+++ src/main/java/com/grupo1/recursos_tic/model/ResourceType.java
@@ -6,11 +6,33 @@ import lombok.RequiredArgsConstructor;
 public enum ResourceType {
 
     BOOK("Libro"),
+    ARTICLE("Artículo"),
+    REPORT("Informe"),
+    DOCUMENTATION("Documentación"),
+    DOCUMENT("Documento"),
+    TUTORIAL("Tutorial"),
+    FAQ("FAQ"),
+    SOFTWARE("Software"),
+    IMAGE("Imagen"),
+    GALLERY("Galería"),
+    AUDIO("Audio"),
+    PODCAST("Podcast"),
     VIDEO("Video"),
-    WEB("Página web");
+    VIDEOCHANNEL("Canal de Video"),
+    TVSHOW("Series de TV"),
+    MOVIE("Película"),
+    WEB("Página web"),
+    BLOG("Blog"),
+    POST("Entrada de blog"),
+    FORUM("Foro"),
+    COMMENT("Comentario"),
+    NEWS("Noticias"),
+    DATABASE("Base de datos"),
+    REPOSITORY("Repositorio"),
+    OTHER("Otro");
 
-    private final String reosurceType;
+    private final String resourceType;
 
     @Override
-    public String toString() { return reosurceType; }
+    public String toString() { return resourceType; }
 }
