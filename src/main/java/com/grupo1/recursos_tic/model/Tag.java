f907e250137a1f90320d50d86011cec41c45bd20
MarinaV <mvallejo89@hotmail.com>
2024-11-20 11:53:46 +0100

    Tag MVC

--- src/main/java/com/grupo1/recursos_tic/model/Tag.java
+++ src/main/java/com/grupo1/recursos_tic/model/Tag.java
@@ -1,10 +1,9 @@
 package com.grupo1.recursos_tic.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.mapping.Map;
+import java.util.HashMap;
 
 @Entity
 @Builder
@@ -13,11 +12,65 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
+
 public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Propiedades
+    private String name;
+
+    private String imageUrl;
+
+    private String description;
+
+
+    @ElementCollection
+    @CollectionTable(name = "tag_resources", joinColumns = @JoinColumn(name = "tag_id"))
+    @MapKeyColumn(name = "resource_id")
+    @Column(name = "resource_name")
+    private Map<Integer, String> resources = new HashMap<>();
+
+    // Getters y setters
+        public Long getId() {return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Map<Integer, String> getResources() {
+            return resources;
+        }
+
+        public void setResources(Map<Integer, String> resources) {
+            this.resources = resources;
+        }
+    }
+
 }
---
+++ src/main/java/com/grupo1/recursos_tic/repository/TagRepository.java
@@ -0,0 +1,8 @@
+package com.grupo1.recursos_tic.repository;
+
+import com.grupo1.recursos_tic.model.Tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
