d7b2265377b3461b64038484ab070e95b5d530a8
KevinZamora <kevin.zamora.amela@gmail.com>
2024-11-21 12:53:58 +0100

    in progress feature / adding show/hide password function

--- src/main/java/com/grupo1/recursos_tic/config/SecurityConfig.java
+++ src/main/java/com/grupo1/recursos_tic/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class  SecurityConfig {
             )
 
             .formLogin(formLogin -> formLogin
+                    .defaultSuccessUrl("/")
                     .failureUrl("/login?error=true")
                     .permitAll()
             )
