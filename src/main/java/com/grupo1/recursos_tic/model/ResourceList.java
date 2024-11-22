0e19ca7e9aa5f16bd7ef8e227d99928762468846
JavGuerra <javguerra@badared.com>
2024-11-22 09:56:05 +0100

    Equals y hashCode

--- src/main/java/com/grupo1/recursos_tic/model/ResourceList.java
+++ src/main/java/com/grupo1/recursos_tic/model/ResourceList.java
@@ -2,8 +2,10 @@ package com.grupo1.recursos_tic.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -42,6 +44,22 @@ public class ResourceList {
         if (this.resources != null) this.resources.remove(resource);
         if (resource != null && resource.getLists() != null)
             resource.getLists().remove(this);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ResourceList that = (ResourceList) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 
 }
