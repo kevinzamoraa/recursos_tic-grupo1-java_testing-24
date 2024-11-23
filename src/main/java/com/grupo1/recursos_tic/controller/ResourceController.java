9c77a04acc50f678a51193bfc098c38a344fc712
JavGuerra <javguerra@badared.com>
2024-11-23 21:17:52 +0100

    Test unitario ResourceController

--- src/main/java/com/grupo1/recursos_tic/controller/ResourceController.java
+++ src/main/java/com/grupo1/recursos_tic/controller/ResourceController.java
@@ -6,6 +6,7 @@ import com.grupo1.recursos_tic.service.RatingService;
 import com.grupo1.recursos_tic.service.ResourceListsService;
 import com.grupo1.recursos_tic.service.ResourceService;
 
+import com.grupo1.recursos_tic.util.ErrMsg;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 import static com.grupo1.recursos_tic.util.Utility.*;
 
@@ -27,11 +27,13 @@ public class ResourceController {
     private ResourceListsService resourceListsService;
     private RatingService ratingService;
 
-    private final String idMsg = "Falta el id o no es un entero positivo";
-    private final String notIdMsg = "El recurso no existe";
-    private final String dataMsg = "Los datos recibidos no son válidos";
-    private final String delMsg = "Error al borrar todos los recursos";
-
+    public static String formValidation(Resource resource) {
+        if (stringIsEmpty(resource.getTitle())) return "Falta el título";
+        if (stringIsEmpty(resource.getUrl())) return "Faltan la URL";
+        if (invalidIntPosNumber((long) resource.getType().ordinal()))
+            return "Falta el tipo de recurso";
+        return null;
+    }
 
     @GetMapping("resources")
     public String findAll(Model model) {
@@ -42,17 +44,17 @@ public class ResourceController {
     @GetMapping("resources/{id}")
     public String findById(Model model, @PathVariable Long id) {
         if (invalidIntPosNumber(id) || id == 0)
-            throw new NoSuchElementException(idMsg);
+            throw new NoSuchElementException(ErrMsg.INVALID_ID);
 
-        if (!resourceService.existsById(id))
-            throw new NoSuchElementException(notIdMsg);
+        Resource resource = resourceService.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(ErrMsg.NOT_FOUND));
 
-        return resourceService.findById(id).map(resource -> {
-            model.addAttribute("resource", resource);
-            if (isAuth()) model.addAttribute("lists", resourceListsService.findByOwnerIdAndResourcesId(userAuth().get().getId(), id));
-            model.addAttribute("ratings", ratingService.findAllByResource_Id(id));
-            return "resource/detail";
-        }).orElseThrow(() -> new NoSuchElementException(notIdMsg));
+        model.addAttribute("resource", resource);
+        model.addAttribute("ratings", ratingService.findAllByResource_Id(id));
+        if (isAuth()) model.addAttribute("lists",
+                resourceListsService.findByOwnerIdAndResourcesId(userAuth().get().getId(), id));
+
+        return "resource/detail";
     }
 
     @GetMapping("resources/create")
@@ -64,49 +66,50 @@ public class ResourceController {
     @GetMapping("resources/create/{listId}")
     public String getFormToCreateNew(Model model, @PathVariable Long listId) {
         if (invalidIntPosNumber(listId) || listId == 0)
-            throw new NoSuchElementException(idMsg);
+            throw new NoSuchElementException(ErrMsg.INVALID_ID);
 
         if (!resourceListsService.existsById(listId))
-            throw new NoSuchElementException(notIdMsg);
+            throw new NoSuchElementException(ErrMsg.NOT_FOUND);
 
         model.addAttribute("resource", new Resource());
         model.addAttribute("listId", listId);
+
         return "resource/form";
     }
 
     @GetMapping("resources/update/{id}")
     public String getFormToUpdate(Model model, @PathVariable Long id) {
         if (invalidIntPosNumber(id) || id == 0)
-            throw new NoSuchElementException(idMsg);
+            throw new NoSuchElementException(ErrMsg.INVALID_ID);
 
-        if (!resourceService.existsById(id))
-            throw new NoSuchElementException(notIdMsg);
+        Resource resource = resourceService.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(ErrMsg.NOT_FOUND));
 
-        return resourceService.findById(id).map(resource -> {
-            model.addAttribute("resource", resource);
-            return "resource/form";
-        }).orElseThrow(() -> new NoSuchElementException(notIdMsg));
+        model.addAttribute("resource", resource);
+        return "resource/form";
     }
 
     @GetMapping("resources/update/{id}/{listId}")
     public String getFormToUpdateAndList(Model model, @PathVariable Long id, @PathVariable Long listId) {
         if (invalidIntPosNumber(id) || id == 0 || invalidIntPosNumber(listId) || listId == 0)
-            throw new NoSuchElementException(idMsg);
+            throw new NoSuchElementException(ErrMsg.INVALID_ID);
 
-        if (!resourceService.existsById(id) || !resourceListsService.existsById(listId))
-            throw new NoSuchElementException(notIdMsg);
+        if (!resourceListsService.existsById(listId))
+            throw new NoSuchElementException(ErrMsg.NOT_FOUND);
 
-        return resourceService.findById(id).map(resource -> {
-            model.addAttribute("resource", resource);
-            model.addAttribute("listId", listId);
-            return "resource/form";
-        }).orElseThrow(() -> new NoSuchElementException(notIdMsg));
+        Resource resource = resourceService.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(ErrMsg.NOT_FOUND));
+
+        model.addAttribute("resource", resource);
+        model.addAttribute("listId", listId);
+
+        return "resource/form";
     }
 
     @PostMapping("resources")
     public String save(Model model, @ModelAttribute Resource resource,
                        @RequestParam(required = false) Long listId) {
-        if (resource == null) throw new NoSuchElementException(dataMsg);
+        if (resource == null) throw new NoSuchElementException(ErrMsg.INVALID_INPUT);
         String error = formValidation(resource);
         if (error != null) throw new NoSuchElementException(error);
 
@@ -127,38 +130,27 @@ public class ResourceController {
                 if (listId != null && listId != 0L)
                     return "redirect:/resourcelists/" + listId;
                 return "redirect:/resources/" + optResource.getId();
-            }).orElseThrow(() -> new NoSuchElementException(notIdMsg));
+            }).orElseThrow(() -> new NoSuchElementException(ErrMsg.NOT_FOUND));
         }
     }
 
     @GetMapping("resources/delete/{id}")
     public String deleteById(Model model, @PathVariable Long id) {
         if (invalidIntPosNumber(id) || id == 0)
-            throw new NoSuchElementException(idMsg);
+            throw new NoSuchElementException(ErrMsg.INVALID_ID);
 
-        if (!resourceService.existsById(id))
-            throw new NoSuchElementException(notIdMsg);
+        Resource resource = resourceService.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(ErrMsg.NOT_FOUND));
 
-        return resourceService.findById(id).map(resource -> {
-            resourceService.removeResourceWithDependencies(resource.getId());
-            return "redirect:/resources";
-        }).orElseThrow(() -> new NoSuchElementException(notIdMsg));
+        resourceService.removeResourceWithDependencies(resource.getId());
+        return "redirect:/resources";
     }
 
     @GetMapping("resources/delete")
     public String deleteAll(Model model) {
         resourceService.deleteAll();
         if (resourceService.count() != 0)
-            throw new NoSuchElementException(delMsg);
+            throw new NoSuchElementException(ErrMsg.NOT_DELETED);
         return "redirect:/resources";
     }
-
-    public static String formValidation(Resource resource) {
-        if (stringIsEmpty(resource.getTitle())) return "Falta el título";
-        if (stringIsEmpty(resource.getUrl())) return "Faltan la URL";
-        if (invalidIntPosNumber((long) resource.getType().ordinal()))
-            return "Falta el tipo de recurso";
-        return null;
-    }
-
 }
